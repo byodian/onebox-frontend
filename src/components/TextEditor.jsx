d204edd467c05d079841614efe6e2a2eb3d97936
byodian <byj6696@gmail.com>
2023-09-10 23:30:24 +0800

    use tiptap

--- src/components/TextEditor.jsx
+++ src/components/TextEditor.jsx
@@ -1,6 +1,10 @@
-import { useState, useEffect } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import Editor from 'ckeditor5-custom-build/build/ckeditor';
+import { useState } from 'react';
+import {
+  EditorContent,
+  useEditor,
+} from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+
 import {
   Button,
   FormControl,
@@ -8,31 +12,27 @@ import {
 } from '@chakra-ui/react';
 import { clearEditorContent, setEditorContent } from '../utils/auth';
 
-const editorConfig = {
-  toolbar: {
-    items: [
-      'bold',
-      'italic',
-      'strikethrough',
-      'underline',
-      'link',
-      'bulletedList',
-      'numberedList',
-      'code',
-      'codeBlock',
-    ],
-  },
-};
-
 function TextEditor({
   handleNoteSubmit, initialContent, folders, initialFolderId, handleError,
 }) {
-  const [content, setContent] = useState(initialContent || '');
+  const editor = useEditor({
+    extensions: [StarterKit],
+    content: initialContent || '',
+    editorProps: {
+      attributes: {
+        class: 'prose prose-base focus:outline-none',
+      },
+    },
+    onUpdate: (props) => {
+      const text = props.editor.getHTML();
+      setEditorContent(text);
+    },
+  });
   const [folderId, setFolderId] = useState(initialFolderId || '');
-  const [isDisabled, setIsDisabled] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
 
   const handleNoteSave = async () => {
+    const content = editor.getHTML();
     setIsLoading(true);
     const requestBody = folderId
       ? { content, folderId }
@@ -41,13 +41,11 @@ function TextEditor({
     try {
       await handleNoteSubmit(requestBody);
       setIsLoading(false);
-      setContent('');
       setFolderId('');
-      setIsDisabled(true);
       clearEditorContent();
+      editor.commands.clearContent();
     } catch (error) {
       setIsLoading(false);
-      setIsDisabled(false);
       handleError(error);
     }
   };
@@ -58,23 +56,14 @@ function TextEditor({
 
   return (
     <div className="py-6">
-      <CKEditor
-        className="ckeditor-test"
-        editor={Editor}
-        config={editorConfig}
-        data={content}
-        onChange={(_event, editor) => {
-          const text = editor.getData();
-          setContent(text);
-          setEditorContent(text);
-
-          if (editor.getData() !== '') {
-            setIsDisabled(false);
-          } else {
-            setIsDisabled(true);
-          }
-        }}
-      />
+      <div
+        className="
+          border-2 border-black rounded max-h-48
+          overflow-auto
+        "
+      >
+        <EditorContent editor={editor} />
+      </div>
       <div className="flex mt-6">
         <FormControl className="mr-10">
           <Select placeholder="选择分类" value={folderId} onChange={handleChange}>
@@ -84,7 +73,7 @@ function TextEditor({
           </Select>
         </FormControl>
         <div className="text-right ml-auto">
-          <Button onClick={handleNoteSave} colorScheme="teal" isDisabled={isDisabled} isLoading={isLoading}>
+          <Button onClick={handleNoteSave} colorScheme="teal" isLoading={isLoading}>
             保存
           </Button>
         </div>
